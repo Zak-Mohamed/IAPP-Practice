0580041f3339d494e30d96599372195e29188fe1
Zak-Mohamed <Zakariya.Mohamed@strathmore.edu>
2025-09-03 10:35:31 +0300

    Progress

--- conf.php
+++ conf.php
@@ -1,18 +1,18 @@
-<?php 
+<?php
 
 // site information 
-$conf ['site_name']  = 'ICS Academy';
-$conf ['site_url']  = 'https://localhost/fol';
-$conf ['site_email']  = 'infor@icsacademy.com';
+$conf['site_name']  = 'ICS Academy';
+$conf['site_url']  = 'https://localhost/IAPP-Practice/';
+$conf['site_email']  = 'info@icsacademy.com';
 
 
- // Site Language 
-$conf ['site_lang']  = 'en';
+// Site Language 
+$conf['site_lang']  = 'en';
 
 
 // Database constants 
-$conf ['DB_TYPE']  = 'mysqli';
-$conf ['DB_HOST']  = 'localhost';
-$conf ['DB_USER']  = 'root';
-$conf ['DB_PASS']  = '';
-$conf ['DB_NAME']  = 'fol';
+$conf['DB_TYPE']  = 'mysqli';
+$conf['DB_HOST']  = 'localhost';
+$conf['DB_USER']  = 'root';
+$conf['DB_PASS']  = '';
+$conf['DB_NAME']  = 'trial';
