20eada589262814a0d2754542d871de5071c1f9e
Zak-Mohamed <Zakariya.Mohamed@strathmore.edu>
2025-09-06 10:54:05 +0300

    Changing config to My Sacco

--- conf.php
+++ conf.php
@@ -1,18 +1,25 @@
 <?php
 
-// site information 
-$conf['site_name']  = 'ICS Academy';
+$conf['sacco_name']  = 'RideLink SACCO';
+$conf['sacco_tagline'] = 'Your Reliable Transport Partner';
 $conf['site_url']  = 'https://localhost/IAPP-Practice/';
-$conf['site_email']  = 'info@icsacademy.com';
+$conf['sacco_email']  = 'info@ridelinksacco.co.ke';
+$conf['sacco_phone'] = '+254 700 000 000';
+$conf['sacco_address'] = 'Nairobi, Kenya';
 
-
-// Site Language 
 $conf['site_lang']  = 'en';
 
+$conf['routes'] = [
+    'Nairobi-CBD' => 'City Center',
+    'Nairobi-Westlands' => 'Westlands',
+    'Nairobi-Karen' => 'Karen',
+    'Nairobi-Rongai' => 'Rongai',
+    'Nairobi-Thika' => 'Thika Road'
+];
 
 // Database constants 
 $conf['DB_TYPE']  = 'mysqli';
 $conf['DB_HOST']  = 'localhost';
 $conf['DB_USER']  = 'root';
 $conf['DB_PASS']  = '';
-$conf['DB_NAME']  = 'trial';
+$conf['DB_NAME']  = 'ridelink_sacco';
